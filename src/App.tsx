8fec0719151d8319285b055466a8e555228a0ec6
UserWindowsXP <wwwebsurf@gmail.com>
2025-05-11 23:14:43 +0200

    new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { useState } from "react";
+import React,{ useState, useEffect } from 'react'
 import "./App.scss";
 import Row from "./components/Row/Row"
 
+type Data = {
+  artist: string
+}
+
 const App = () => {
-  const targetWord = "REACT";
   const maxAttempts = 6;
+  const [error, setError] = useState<boolean>(false)
+  const [artist, setArtist] = useState<string | null>(null)
+  const targetWord: string | null = artist ? artist.toLocaleUpperCase() : null;
+
+  const gamePlayable = !targetWord || error
+
+  const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
+  const fetchArtist = async() => {
+    try{
+      const response = await fetch(`${apiUrl}/randomArtist`)
+      console.log(response)
+      if(!response.ok) throw new Error()
+      const data:Data = await response.json()
+      setArtist(data.artist)
+    } catch (error) {
+      setError(true)
+    }
+  }
 
   const [guesses, setGuesses] = useState<string[]>([]);
   const [currentGuess, setCurrentGuess] = useState("");
@@ -16,10 +37,7 @@ const App = () => {
   };
 
   const handleGuess = () => {
-    if (currentGuess.length !== 5) {
-      return;
-    }
-
+    if(gamePlayable) return 
     const updatedGuesses:string[] = [...guesses, currentGuess];
     setGuesses(updatedGuesses);
 
@@ -28,26 +46,45 @@ const App = () => {
     }
   };
 
+  const empltyRows = [];
+  for (let i = 0; i < 6 - guesses.length; i++) {
+    if(!targetWord) continue
+    empltyRows.push(<Row key={i}  targetWord={targetWord} />);
+  }
+
+  useEffect(() =>{
+    fetchArtist()
+  }, [])
+
   return (
     <div className="main">
-      <h1>Wordle</h1>
-      {guesses.map((guess, index) => (
-        <Row key={index} guess={guess} targetWord={targetWord} />
-      ))}
-      {!isGameOver && (
-        <>
-          <input
-            className="main__input"
-            onChange={handleInputChange}
-            maxLength={targetWord.length}
-            placeholder="Enter your guess"
-          />
-          <button onClick={handleGuess} className="main__button">Guess</button>
-        </>
-      )}
-      {isGameOver && currentGuess !== targetWord && (
-        <p>{`Game over! The word was: ${targetWord}`}</p>
-      )}
+      { gamePlayable || targetWord && ( 
+      <>
+        <h1 className='main__title'>Wordle - {artist}</h1>
+        <p className='main__description'>Guess the artist or band 🎵</p>
+        <p className='main__subtitle'>Type as many letters as you want </p>
+        {guesses.map((guess, index) => (
+          <Row key={index} guess={guess} targetWord={targetWord} />
+        ))}
+        {empltyRows}
+        {!isGameOver && (
+          <>
+            <input
+              className="main__input"
+              onChange={handleInputChange}
+              maxLength={targetWord.length}
+              placeholder="Enter your guess"
+            />
+            <button onClick={handleGuess} className="main__button">Guess</button>
+          </>
+        )}
+        {/* GAME OVER MESSAGE */}
+        {isGameOver && currentGuess !== targetWord && (
+          <p>{`Game over 🫣 The word was: ${artist}`}</p>
+        )}
+      </>)}
+      {/* ERROR */}
+      {error &&( <p>We are sorry, an error has occurred. It's not possible to play now 😔.</p>)}
     </div>
   );
 };
--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,29 +1,36 @@
 import "./Row.scss";
 
 type RowProps = {
-  guess: string,
+  guess?: string,
   targetWord: string
 }
 
-const Row = ({ guess, targetWord }:RowProps) => {
-  const getLetterStatus = (letter: string, index: number) => {
-    if (letter === targetWord[index]) {
-      return "correct";
-    } else if (targetWord.includes(letter)) {
-      return "present";
-    } else return "absent";
-  }
+const Row = ({ guess, targetWord }: RowProps) => {
+  if (!targetWord) return null;
 
-  const guessArray = guess.split("");
+  const targetArray = targetWord.split("");
+  const guessArray = guess ? guess.split("") : [];
+
+  const getLetterStatus = (index: number) => {
+    const letter = targetArray[index];
+    const guessedLetter = guessArray[index];
+
+    if (letter === ' ') return "space";
+    if (!guessedLetter) return "absent";
+
+    if (guessedLetter === letter) return "correct";
+    if (targetArray.includes(guessedLetter)) return "present";
+    return "absent";
+  };
 
   return (
     <div className="word-row">
-      {guessArray.map((letter, index) => (
+      {targetArray.map((letter, index) => (
         <span
-          className={`word-row__letter ${getLetterStatus(letter, index)}`}
+          className={`word-row__letter ${getLetterStatus(index)}`}
           key={index}
         >
-          {letter}
+          {letter === ' ' ? ' ' : (guessArray[index] || '')}
         </span>
       ))}
     </div>
