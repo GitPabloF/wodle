ae5b86d77a6f1fe02ef6f94e1574765251a36f48
GitPabloF <pablofitoussi@gmail.com>
2025-05-16 08:41:31 +0200

    won message + cleaning process

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React,{ useState, useEffect } from 'react'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 import "./App.scss";
 import loader from './assets/loader.svg'
 import Row from "./components/Row/Row"
@@ -8,15 +10,24 @@ type Data = {
 }
 
 const App = () => {
-  const maxAttempts = 6;
-  const [error, setError] = useState<boolean>(false)
-  const [artist, setArtist] = useState<string | null>(null)
-  const [isLoading, setIsLoading] = useState<boolean>(true)
-  const targetWord: string | null = artist ? artist.toLocaleUpperCase() : null;
-
-  const gamePlayable = !targetWord || error
-
+  const { width, height } = useWindowSize()
   const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
+  const maxAttempts = 6;
+  const emptyRows = [];
+
+  const [error, setError] = useState(false)
+  const [artist, setArtist] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isGameWon, setIsGameWon] = useState(false)
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const targetWord: string | null = artist ? artist.toLocaleUpperCase() : null;
+  const gamePlayable = !!targetWord && !error && !isGameOver && !isGameWon;
+
 
   const fetchArtist = async() => {
     try{
@@ -25,34 +36,39 @@ const App = () => {
       const data:Data = await response.json()
       setArtist(data.artist)
       setIsLoading(false)
-    } catch (error) {
+    } catch {
       setError(true)
+      setIsLoading(false)
+    } finally {
       setIsLoading(false)
     }
   }
 
-  const [guesses, setGuesses] = useState<string[]>([]);
-  const [currentGuess, setCurrentGuess] = useState("");
-  const [isGameOver, setIsGameOver] = useState(false);
-
   const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setCurrentGuess(event.target.value.toUpperCase());
+    const value = event.target.value.toUpperCase()
+    setInputValue(value);
+    setCurrentGuess(value)
   };
 
   const handleGuess = () => {
-    if(gamePlayable) return 
+    console.log('handleGuess');
+    if(!gamePlayable) return 
+    setInputValue('')
+    console.log('guesses', guesses)
+    console.log(currentGuess, 'currentGuees')
     const updatedGuesses:string[] = [...guesses, currentGuess];
     setGuesses(updatedGuesses);
 
-    if (currentGuess === targetWord || updatedGuesses.length >= maxAttempts) {
+    if(currentGuess === targetWord ) setIsGameWon(true);
+   
+    if (updatedGuesses.length >= maxAttempts) {
       setIsGameOver(true);
     }
   };
 
-  const empltyRows = [];
   for (let i = 0; i < 6 - guesses.length; i++) {
     if(!targetWord) continue
-    empltyRows.push(<Row key={i}  targetWord={targetWord} />);
+    emptyRows.push(<Row key={i}  targetWord={targetWord} />);
   }
 
   useEffect(() =>{
@@ -61,30 +77,41 @@ const App = () => {
 
   return (
     <div className="main">
-      { gamePlayable || targetWord && ( 
+      {targetWord && ( 
       <>
-        <h1 className='main__title'>WODLE</h1>
+        <h1 className='main__title'>WODLE - {isGameOver} - {isGameWon}</h1>
         <p className='main__description'>Guess the artist or band 🎵</p>
         <p className='main__subtitle'>Type as many letters as you want </p>
         <div className='main__rows-wrapper'>
         {guesses.map((guess, index) => (
           <Row key={index} guess={guess} targetWord={targetWord} />
         ))}
-        {empltyRows}
+        {emptyRows}
         </div>
-        {!isGameOver && (
+        {gamePlayable && (
           <>
             <input
               className="main__input"
               onChange={handleInputChange}
+              value={inputValue}
               maxLength={targetWord.length}
               placeholder="Enter your guess"
             />
             <button onClick={handleGuess} className="main__button">Guess</button>
           </>
         )}
+        {/* winning message */}
+        {isGameWon && (
+          <>
+          <Confetti
+            width={width}
+            height={height}
+          />
+          <p>Well played! 🤘 You guessed it!</p>
+          </>
+        )}
         {/* GAME OVER MESSAGE */}
-        {isGameOver && currentGuess !== targetWord && (
+        {isGameOver && !isGameWon && (
           <p>{`Game over 🫣 The word was: ${artist}`}</p>
         )}
       </>)}
